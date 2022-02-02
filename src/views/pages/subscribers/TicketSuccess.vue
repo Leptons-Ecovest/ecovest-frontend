47b68905a151f9da71549e23936ec3eded173f5b
vicSystems <victorasuquob@gmail.com>
2022-02-02 15:27:53 +0100

    modal error

--- src/views/pages/subscribers/TicketSuccess.vue
+++ src/views/pages/subscribers/TicketSuccess.vue
@@ -4,7 +4,7 @@
             <div class="nk-block-head nk-block-head-lg">
                 <div class="nk-block-between-md g-4">
                     <div class="nk-block-head-content">
-                        <h2 class="nk-block-title fw-normal text-center">Ticket Submitted Successfully</h2>
+                       
                           <router-link v-if="admin" to="/admin/add-project" class="btn btn-primary">Add Project</router-link>
                        
                     </div>
@@ -14,7 +14,7 @@
         </div>
 
         <div class="container">
-
+ <h2 class=" text-center">Ticket Submitted Successfully</h2>
             
         <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_9n1h4nww.json"  background="transparent"  speed="1"  style="max-width: 400px; max-height: 400px; "  class="mx-auto text-center"   autoplay></lottie-player>
 
