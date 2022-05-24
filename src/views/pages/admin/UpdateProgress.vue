2149640cfd866ae226289754f9ba7f010e670033
vicSystems <victorasuquob@gmail.com>
2022-05-24 10:19:44 +0100

    display single block

--- src/views/pages/admin/UpdateProgress.vue
+++ src/views/pages/admin/UpdateProgress.vue
@@ -5,6 +5,7 @@
                 <div class="nk-block-between-md g-4">
                     <div class="nk-block-head-content">
                         <h2 class="nk-block-title fw-normal">Update Progress</h2>
+                        <h6>Subscriber details:</h6>
                        
                     </div>
                 </div>
@@ -14,7 +15,33 @@
 
         <div class="container py-3">
 
-            
+
+            <div class="card mb-3 border shadow">
+                <div class="card-body">
+                    <h6>Project Title</h6>
+                    <p>Description of work done</p>
+                    <div class="media">
+                        <p class="text-muted">media files...</p>
+                    </div>
+                    <span class="float-right">2 days ago</span>
+                </div>
+            </div>
+
+
+            <h4 class="py-3">New Report.</h4>
+
+
+          <div class="form-group ">
+                <input type="file" class="file-input">
+          </div>
+          <div class="form-group">
+              <textarea name="description" class="form-control" placeholder="Description of progress of work" id="" cols="30" rows="5"></textarea>
+          </div>
+          <div class="form-group">
+              <button class="btn btn-success">Update</button>
+          </div>
+
+
 
             
 
