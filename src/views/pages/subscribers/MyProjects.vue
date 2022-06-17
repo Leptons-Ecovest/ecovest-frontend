3438c998870cee6a3d3b4ee18423f4325b765f4b
vicSystems <victorasuquob@gmail.com>
2022-06-17 10:34:20 +0100

    my projects page

--- src/views/pages/subscribers/MyProjects.vue
+++ src/views/pages/subscribers/MyProjects.vue
@@ -15,23 +15,23 @@
         </div>
 
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
+            <div class="">
+                <div class="">
 
-                    <ul class="nav nav-pills mb-3 nav-fill" id="pills-tab" role="tablist">
-                        <li class="nav-item" role="presentation">
+                    <ul class="nav nav-pills mb-3 nav-fill card-body shadow row" id="pills-tab" role="tablist">
+                        <li class="nav-item col-md-4" role="presentation">
                             <a class="nav-link active" id="pills-home-ab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Project Details</a>
                         </li>
-                        <li class="nav-item" role="presentation">
+                        <li class="nav-item col-md-4" role="presentation">
                             <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Payment History</a>
                         </li>
-                        <li class="nav-item" role="presentation">
+                        <li class="nav-item col-md-4" role="presentation">
                             <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Project Status</a>
                         </li>   
                     </ul>
 
                 </div>  
-                <div class="col-md-12">
+                <div class="">
                     
                     <div class="tab-content" id="pills-tabContent">
                         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
