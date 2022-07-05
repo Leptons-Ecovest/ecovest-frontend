afc517d14def1e467d4ef500f6f1302527d80756
vicSystems <victorasuquob@gmail.com>
2022-07-05 20:28:49 +0100

    courses

--- src/views/pages/admin/UpdateProgress.vue
+++ src/views/pages/admin/UpdateProgress.vue
@@ -5,7 +5,8 @@
                 <div class="nk-block-between-md g-4">
                     <div class="nk-block-head-content">
                         <h2 class="nk-block-title fw-normal">Update Progress</h2>
-                        <h6>Subscriber details:</h6>
+                        <h6>Subscriber details: {{reports[0]?.subscribers.name}}</h6>
+                        <h6>Project details: {{reports[0]?.payment_plan.building_project.title}}</h6>
                        
                     </div>
                 </div>
@@ -15,118 +16,135 @@
 
         <div class="container py-3">
 
+             
 
-            <div v-for="report,index in reports" :key="index.id" class="card mb-3 border shadow">
-                <div class="card-body">
-                    <h6>{{report.payment_plan.building_project.title}}</h6>
-                    <p>{{report.description}}</p>
-                    <p>{{report.issues}}</p>
-                    <p class="badge badge-warning">{{report.stage}} {{report.percentage_completion}}%</p>
-                    
-                    <div class="media">
-                        <p class="text-muted">media files...</p>
-                    </div>
-                    <div class="assetx d-flex justify-content-start flex-wrap">
-                        <div v-for="image,index in report.assets" :key="index.id" class="p-1">
-                            <img style="max-height: 120px; max-width: 120px;" :src="image.media_url" alt="">
+            <div class="row">
+                <div style="height: 440px; overflow: scroll;" class="col-md-6">
+                 <h4 class="py-3">Reports</h4>
+                    <div v-for="report,index in reports" :key="index.id" class="card mb-3 border shadow">
+                        <div class="card-body">
+                            <h6>Description:</h6>
+                            <p>{{report.description_work}}</p>
+                            <h6>Issues:</h6>
+                            <p>{{report.issues}}</p>
+                            <p class="badge badge-warning">{{report.stage}}</p>
+                            
+                           
+                            <div class="assetx d-flex justify-content-start flex-wrap">
+                                <div v-for="image,index in report.assets" :key="index.id" class="p-1">
+                                    <img style="max-height: 120px; max-width: 120px;" :src="image.media_url" alt="">
+                                </div>
+        
+                            </div>
+                            <span class="font-weight-bold">Percentage Completion: {{report.percentage_completion}}%</span>
+                            <span class="float-right">{{report.created_at}}</span>
                         </div>
-
                     </div>
-                    <span class="float-right">2 days ago</span>
+
                 </div>
-            </div>
+                <div class="col-md-6">
 
-
-            <h4 class="py-3">New Report.</h4>
-
-                  <div  class="col-md-12 ">
-
-                    <div  class=" d-flex flex-wrap justify-content-start">
-                        <div id="previewImg" class="">
-
+                    <div  class=" ">
+    
+                          <h4 class="py-3">New Report.</h4>
+                          
+                        <div  class=" d-flex flex-wrap justify-content-start">
+                            <div id="previewImg" class="">
+    
+                            </div>
                         </div>
-                    </div>
+    
+    
+                                                <div class="form-group mx-auto text-center">
+    
+    
+    
+                                                
+    
+                                                      <!-- <img 
+                                                    onclick="document.getElementById('customFile').click()" 
+                                                    id="previewImg" 
+                                                    style="height: 189px; width: 189px; object-fit: cover;" 
+                                                    class="shadow border border-primary" 
+                                                    :src="avatar??'https://eu.ui-avatars.com/api/?name='+name"
+                                                     >
+    
+                                                     
+                                                      <img 
+                                                    onclick="document.getElementById('customFile').click()" 
+                                                    id="previewImg2" 
+                                                    style="height: 189px; width: 189px; object-fit: cover;" 
+                                                    class="shadow border border-primary d-none" 
+                                                    src=""
+                                                     > -->
+    
+                                                     
+    
+                                                    <div class="text-center py-3">
+                                                        <input @change="previewFile4" ref="file" type="file" id="customFile"  multiple> 
+                                                    </div>
+    
+    
+                                                </div> 
 
 
-                                            <div class="form-group mx-auto text-center">
+                                        <div class="form-group">
+                                            <label for="">Description</label>
+                                            <textarea v-model="description" name="description" class="form-control" placeholder="Description of progress of work" id="" cols="30" rows="5"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Issues</label>
+                                            <textarea v-model="issues" name="description" class="form-control" placeholder="Description of progress of work" id="" cols="30" rows="5"></textarea>
+                                        </div>
 
-
-
-                                            
-
-                                                  <!-- <img 
-                                                onclick="document.getElementById('customFile').click()" 
-                                                id="previewImg" 
-                                                style="height: 189px; width: 189px; object-fit: cover;" 
-                                                class="shadow border border-primary" 
-                                                :src="avatar??'https://eu.ui-avatars.com/api/?name='+name"
-                                                 >
-
-                                                 
-                                                  <img 
-                                                onclick="document.getElementById('customFile').click()" 
-                                                id="previewImg2" 
-                                                style="height: 189px; width: 189px; object-fit: cover;" 
-                                                class="shadow border border-primary d-none" 
-                                                src=""
-                                                 > -->
-
-                                                 
-
-                                                <div class="text-center py-3">
-                                                    <input @change="previewFile4" ref="file" type="file" id="customFile"  multiple> 
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label for="">Stage</label>
+                                                    <select v-model="stage" id="" class="form-control">
+                                                        <option value="Foundation">Foundation</option>
+                                                        <option value="Slab">Slab</option>
+                                                        <option value="Interior Design">Interior Design</option>
+                                        
+                                                    </select>
                                                 </div>
 
+                                            </div>
+                                            <div class="col-md-6">
 
-                                            </div> 
+                                                <div class="form-group">
+                                                <label for="">Percentage Completion</label>
+                                                <select v-model="percent" name="" id="" class="form-control">
+                                                    <option value="10">10</option>
+                                                    <option value="20">20</option>
+                                                    <option value="60">60</option>
+                                                    <option value="80">80</option>
+                                                    <option value="100">100</option>
+                                                </select>
+                                                </div>
+                                                
+                                            </div>
                                         </div>
 
 
+
+
+                                        <div class="form-group py-3">
+                                            <button @click="submitReport()" class="btn btn-success float-right">Update</button>
+                                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+
+
+
+
+
+
          
-          <div class="form-group">
-            <label for="">Description</label>
-              <textarea v-model="description" name="description" class="form-control" placeholder="Description of progress of work" id="" cols="30" rows="5"></textarea>
-          </div>
-          <div class="form-group">
-            <label for="">Issues</label>
-              <textarea v-model="issues" name="description" class="form-control" placeholder="Description of progress of work" id="" cols="30" rows="5"></textarea>
-          </div>
 
-          <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="">Stage</label>
-                    <select v-model="stage" id="" class="form-control">
-                        <option value="Foundation">Foundation</option>
-                        <option value="Slab">Slab</option>
-                        <option value="Interior Design">Interior Design</option>
-        
-                    </select>
-                </div>
-
-            </div>
-            <div class="col-md-6">
-
-                <div class="form-group">
-                  <label for="">Percentage Completion</label>
-                  <select v-model="percent" name="" id="" class="form-control">
-                      <option value="10">10</option>
-                      <option value="20">20</option>
-                      <option value="60">60</option>
-                      <option value="80">80</option>
-                      <option value="100">100</option>
-                  </select>
-                </div>
-                
-            </div>
-          </div>
-
-
-
-
-          <div class="form-group py-3">
-              <button @click="submitReport()" class="btn btn-success float-right">Update</button>
-          </div>
 
 
 
@@ -155,7 +173,8 @@ export default {
             description: '',
             issues: '',
             stage: '',
-            percent: ''
+            percent: '',
+            userDatax: []
         }
     },
     methods: {
@@ -177,11 +196,11 @@ export default {
 
             console.log(this.media)
 
-            alert(total_file)
+            
 
              for(var i=0; i<total_file; i++)
             {
-                alert(event.target.files[i].type)
+                
 
 
 
@@ -217,7 +236,7 @@ export default {
             }
 
 
-            formData.append('building_project_id', this.$route.params.id)
+            formData.append('payment_plan_id', this.$route.params.id)
 
             formData.append('description', this.description)
             formData.append('issues', this.issues)
@@ -251,6 +270,8 @@ export default {
 
 
                 console.log(response)
+
+                this.getReports()
             })
             .catch((response)=>{
 
@@ -261,10 +282,13 @@ export default {
 
         getReports(){
 
-            this.axios({
-                url: process.env.VUE_APP_URL+'/api/progress_report',
-                method: 'get',
 
+            this.axios({
+                url: process.env.VUE_APP_URL+'/api/get_reports',
+                method: 'post',
+                data:{
+                    payment_plan_id: this.$route.params.id
+                },
                 headers: {
                     'Accept': 'application/json',
                     'Authorization': 'Bearer ' +localStorage.getItem('user_token')
@@ -273,26 +297,32 @@ export default {
             })
             .then((response)=>{
 
-
+                 console.log(response)
 
                 this.reports = response.data
 
 
-                console.log(response)
+               
             })
             .catch((response)=>{
 
                 console.log(response)
             })
             
-        }
+        },
+
+
+
 
         
     },
 
     mounted() {
 
+        this.userDatax = JSON.parse(localStorage.getItem('user_data'));
+
         this.getReports()
+        
     },
 }
 </script>
