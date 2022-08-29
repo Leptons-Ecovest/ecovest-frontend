98d578c2db033f86fe1bbdb622d382c23eaab415
vicSystems <victorasuquob@gmail.com>
2022-08-29 12:05:48 +0100

    redirect

--- src/views/pages/admin/MembersSubscriptions.vue
+++ src/views/pages/admin/MembersSubscriptions.vue
@@ -32,9 +32,9 @@
                         <td>
                             <img style="width: 54px; height: 54px; border-radius: 50%;" :src="getImgUrl(user_plan?.user?.avatar)" alt="">
                         </td>
-                        <td>{{user_plan.user.name}}</td>
-                        <td>{{user_plan.user.email}}</td>
-                        <td>{{user_plan.building_project?.title}}</td>
+                        <td>{{user_plan?.user.name}}</td>
+                        <td>{{user_plan?.user.email}}</td>
+                        <td>{{user_plan?.building_project?.title}}</td>
                         <td>
                             <button @click="loadPaymentSchedule(user_plan.id)" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                                 Details
