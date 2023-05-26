eb9d865d0dc6d9d0fe6b030d3454836e8c2564af
Chigozirim <iamokwukwe@gmail.com>
2023-05-26 14:50:18 +0100

    Replace onClick function for 'Submit' button with one that passes the form data to the next page

--- src/views/pages/admin/CreatePlan2.vue
+++ src/views/pages/admin/CreatePlan2.vue
@@ -1,87 +1,89 @@
 <template>
-   <div class="nk-content-body">
-        <div class="nk-content-wrap">
-            <div class="nk-block-head nk-block-head-lg">
-                <div class="nk-block-between-md g-4">
-                    <div class="nk-block-head-content">
-                        <h2 class="nk-block-title fw-normal">Create Payment Plan</h2>
-                       
-                    </div>
-                </div>
-            </div><!-- .nk-block-head -->
-         
-        </div>
-
-        <div class="container py-3">
-
-            <div class="row">
-
-                <div class="col-md-12">
-
-                    <h6 class="text-center py-3">Step 1/4</h6>
-
-                    <div class="form-group">
-                        <label >Building Projects</label>
-                        <select class="form-control" v-model="building_project_title" >
-
-                   
-
-                            <option v-for="building_project in building_projects" :key="building_project.index" >{{building_project.title}}</option>
-
-                        </select>
-                    </div>
-
-                    <div class="form-group d-none">
-                        <label >Subscribers Email</label>
-                        <input type="text" class="form-control" v-model="subscribers_email" placeholder="Enter Subscriber email">
-                    </div>
-
-                    <div class="form-group">
-                        <label >Date</label>
-                        <input type="date" class="form-control" v-model="start_date" placeholder="">
-                    </div>
-
-                    <div class="form-group">
-                        <label >Description</label>
-                        <textarea  row="5" type="text" class="form-control" v-model="description" placeholder="Lets have some more details about the project for this user"></textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <button @click="create_payment_plan()" class="btn btn-primary">Submit</button>
-                    </div>
-
-           
-
-
-
-
-                </div>
-
-                
-
-           
-
-
-            </div>
-
-
-
-
-
-        </div>
-
-      
-        <!-- footer @s -->
-      
-        <!-- footer @e -->
-    </div>
-</template>
+    <div class="nk-content-body">
+         <div class="nk-content-wrap">
+             <div class="nk-block-head nk-block-head-lg">
+                 <div class="nk-block-between-md g-4">
+                     <div class="nk-block-head-content">
+                         <h2 class="nk-block-title fw-normal">Create Payment Plan</h2>
+                        
+                     </div>
+                 </div>
+             </div><!-- .nk-block-head -->
+          
+         </div>
+ 
+         <div class="container py-3">
+ 
+             <div class="row">
+ 
+                 <div class="col-md-12">
+ 
+                     <h6 class="text-center py-3">Step 1/4</h6>
+ 
+                     <div class="form-group">
+                         <label >Building Projects</label>
+                         <select class="form-control" v-model="building_project_title" >
+ 
+                    
+ 
+                             <option v-for="building_project in building_projects" :key="building_project.index" >{{building_project.title}}</option>
+ 
+                         </select>
+                     </div>
+ 
+                     <div class="form-group d-none">
+                         <label >Subscribers Email</label>
+                         <input type="text" class="form-control" v-model="subscribers_email" placeholder="Enter Subscriber email">
+                     </div>
+ 
+                     <div class="form-group">
+                         <label >Date</label>
+                         <input type="date" class="form-control" v-model="start_date" placeholder="">
+                     </div>
+ 
+                     <div class="form-group">
+                         <label >Description</label>
+                         <textarea  row="5" type="text" class="form-control" v-model="description" placeholder="Lets have some more details about the project for this user"></textarea>
+                     </div>
+ 
+                     <div class="form-group">
+                         <button @click="next_stage()" class="btn btn-primary">Submit</button>
+                     </div>
+ 
+            
+ 
+ 
+ 
+ 
+                 </div>
+ 
+                 
+ 
+            
+ 
+ 
+             </div>
+ 
+ 
+ 
+ 
+ 
+         </div>
+ 
+       
+         <!-- footer @s -->
+       
+         <!-- footer @e -->
+     </div>
+ </template>
+ 
 <script>
 import moment from "moment";
 
-import { useToast } from 'vue-toastification'
+// import { useToast } from 'vue-toastification'
 
-const toast = useToast()
+// const toast = useToast()
+
 
 
 export default {
@@ -153,57 +155,17 @@ export default {
 
         },
 
-        create_payment_plan(){
-
-            // alert(this.building_project_title)
-            // alert(this.subscribers_email)
-            // alert(this.start_date)
-            // alert(this.description)
-
-                let loader = this.$loading.show({
-                    // Optional parameters
-                    container: this.fullPage ? null : this.$refs.formContainer,
-                    canCancel: false,
-                    onCancel: this.onCancel,
-                    color: '#6CC3EC',
-                });
-
-            this.axios({
-                    method: "post",
-                    url: process.env.VUE_APP_URL+'/api/create_payment_plan',
-                    data: {
-                        start_date: this.start_date,
-                        subscribers_email: this.user_data.email,
-                        building_project_title: this.building_project_title,
-                        description: this.description,
-                        duration: 12
-
-                    },
-                    headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-type': 'application/json',
-                    'Accept': 'application/json',
-                    'Authorization': 'Bearer ' +localStorage.getItem('user_token')
-                },
-            })
-            .then((response)=>{
-
-                 loader.hide()
-                    toast.success('Plan created successfully!!');
-                console.log(response)
-
-                return this.$router.push('/admin/create-payment-schedule/'+response.data.payment_plan.id);
-            })
-            .catch((response)=>{
-
-                     loader.hide()
-                    toast.error('An error occured');
-                console.log(response)
-            })
-
+        next_stage() {
+            return this.$router.push({
+                path: '/admin/create-payment-schedule/',
+                query: {
+                    start_date: this.start_date,
+                    subscribers_email: this.user_data.email,
+                    building_project_title: this.building_project_title,
+                    description: this.description,
+                }
+            });
         },
-
-
      
     },
 
